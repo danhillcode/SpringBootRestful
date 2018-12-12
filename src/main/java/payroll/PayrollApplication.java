21be24141f479d08b63e895a5669564b3d007a96
danhillcode <danhilltravel@hotmail.co.uk>
2018-12-12 16:38:04 +0000

    some errors with hibernate needs fixing

---
+++ src/main/java/payroll/PayrollApplication.java
@@ -0,0 +1,15 @@
+package payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://start.spring.io/
+//https://spring.io/guides/tutorials/rest/
+
+@SpringBootApplication
+public class PayrollApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(PayrollApplication.class, args);
+    }
+}
